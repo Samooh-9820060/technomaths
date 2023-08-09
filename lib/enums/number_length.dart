d7333c89a026ae0a3ba45b1eebd1bf4acb670c6c
Samooh-9820060 <114355897+Samooh-9820060@users.noreply.github.com>
2023-08-09 21:45:55 +0500

    game logic updated
    number length enum deleted
    options getting logic updated
    difficulty increasing logic updated

--- lib/enums/number_length.dart
+++
@@ -1,7 +0,0 @@
-enum NumberLength {
-  one,  // for example, numbers in range 1-10
-  two, // for example, numbers in range 10-100
-  three, // for example, numbers in range 100-1000
-  four,
-  five
-}
