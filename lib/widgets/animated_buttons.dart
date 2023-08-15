1577174d6d9d307bcde8ae9be5b149dfe63c9dcf
Samooh-9820060 <114355897+Samooh-9820060@users.noreply.github.com>
2023-08-15 17:24:12 +0500

    Revert "settings theme updates"
    This reverts commit 6ac86bff50fb5b26384d1f98aee4946a1bd1f517.

--- lib/widgets/animated_buttons.dart
+++ lib/widgets/animated_buttons.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../themes/theme_data.dart';
 
 class AnimatedButton extends StatelessWidget {
   final String text;
@@ -10,20 +9,6 @@ class AnimatedButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ThemeData currentTheme = Theme.of(context);
-    // Determine which custom theme is currently active
-    CustomTheme? activeTheme;
-    for (var theme in AppThemes.themes.entries) {
-      if (theme.value.themeData.primaryColor == currentTheme.primaryColor) {
-        activeTheme = theme.key;
-        break;
-      }
-    }
-
-    LinearGradient gradient = activeTheme != null
-        ? AppThemes.buttonGradients[activeTheme]!
-        : LinearGradient(colors: [Colors.grey, Colors.grey]);  // Default to a grey gradient if theme is undetermined
-
     double screenWidth = MediaQuery.of(context).size.width;
     double fontSize = screenWidth < 360 ? 16 : 20;
 
@@ -33,7 +18,12 @@ class AnimatedButton extends StatelessWidget {
         width: screenWidth * 0.5,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          gradient: gradient,
+          gradient: LinearGradient(
+            colors: [Colors.deepPurple, Colors.blueAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+
           boxShadow: [
             BoxShadow(
               color: Colors.black.withOpacity(0.3),
@@ -42,31 +32,21 @@ class AnimatedButton extends StatelessWidget {
             ),
           ],
         ),
-        child: Material(
-          color: Colors.transparent,
-          child: InkWell(
-            onTap: onPressed,
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              alignment: Alignment.center,
-              child: FittedBox(
-                fit: BoxFit.fitWidth,
-                child: Text(
-                  text,
-                  style: GoogleFonts.fredoka(
-                    fontSize: fontSize,
-                    color: currentTheme.colorScheme.onPrimary,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2, 2),
-                        blurRadius: 3.0,
-                        color: Color.fromARGB(255, 0, 0, 0),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            primary: Colors.transparent,
+            shadowColor: Colors.transparent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          onPressed: onPressed,
+          child: FittedBox(
+            fit: BoxFit.fitWidth,
+            child: Text(
+              text,
+              style: GoogleFonts.fredoka(fontSize: fontSize, color: Colors.white),
             ),
           ),
         ),
@@ -74,3 +54,4 @@ class AnimatedButton extends StatelessWidget {
     );
   }
 }
+
