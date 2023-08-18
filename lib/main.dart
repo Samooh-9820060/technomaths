5f40d76486f3b7ff2c0c3ac68a2736064b036773
Samooh-9820060 <114355897+Samooh-9820060@users.noreply.github.com>
2023-08-18 10:41:45 +0500

    wall of fame update
    table design updated to flex

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:technomaths/screens/home_screen.dart';
 
@@ -8,6 +9,7 @@ void main() async {
   await Firebase.initializeApp();
   MobileAds.instance.initialize();
   runApp(const MyApp());
+  //debugPaintSizeEnabled = true;
 }
 
 class MyApp extends StatelessWidget {
@@ -23,6 +25,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomeScreen(),
       debugShowCheckedModeBanner: false,
+
     );
   }
 }
