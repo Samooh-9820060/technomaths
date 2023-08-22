eed3d5e4cac04b955e6c102a44f6959d1b76c203
Samooh-9820060 <114355897+Samooh-9820060@users.noreply.github.com>
2023-08-22 23:17:37 +0500

    levels screen created
    design for levels screen has been created

--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -11,7 +11,7 @@ final CustomTheme lightTheme = CustomTheme(
     brightness: Brightness.light,
     // add other theme data you need
   ),
-  textColor: Colors.blueAccent,
+  textColor: Colors.blue[800]!,
   btnTextColor: Colors.white,
   headerColor: Colors.blueAccent,
   iconColor: Colors.blueAccent,
@@ -29,29 +29,33 @@ final CustomTheme lightTheme = CustomTheme(
 
 final CustomTheme darkTheme = CustomTheme(
   themeData: ThemeData(
-    primaryColor: Colors.teal[800]!,  // A muted teal color for primary
+    primaryColor: Colors.black,  // Pure black for primary color
     colorScheme: ColorScheme.dark(
-      primary: Colors.teal[600]!,
-      secondary: Colors.deepPurple,  // A muted purple for secondary
+      primary: Colors.teal[700]!,  // A deeper teal for primary in dark mode
+      secondary: Colors.purple[700]!,  // A deeper purple for secondary
+      background: Colors.black,  // Dark background
+      surface: Colors.grey[900]!, // Slightly lighter surface color
     ),
     brightness: Brightness.dark,
+    //canvasColor: Colors.black,
     // add other theme data you need
   ),
-  textColor: Colors.tealAccent[200]!,
-  btnTextColor: Colors.white,  // A lighter teal color for button text
-  headerColor: Colors.tealAccent[200]!,
-  iconColor: Colors.tealAccent[200]!,
-  btnTextColorReverse: Colors.black,      // Kept the same
-  errorColor: Colors.red,                 // Universal indicator
-  positiveColor: Colors.green,            // Universal indicator
-  progressBarBackground: Colors.grey[200]!,  // Darker shade for dark mode
-  positiveColorLight: Colors.green[400]!,   // Kept the same
-  positiveColorDark: Colors.green[700]!,    // Kept the same
-  backdropBackgroundColor: Colors.grey[850]!,  // A very dark grey for backdrop
-  bestScoreBackground: Colors.deepPurple[600]!,    // A dark shade of amber
-  buttonIndicatorColor: Colors.amberAccent,   // Amber accent for indicator
-  tableSurroundColor: Colors.grey[800]!,
+  textColor: Colors.tealAccent[400]!,  // Brighter teal accent for text
+  btnTextColor: Colors.white,
+  headerColor: Colors.tealAccent[400]!,
+  iconColor: Colors.tealAccent[400]!,
+  btnTextColorReverse: Colors.grey[300]!,  // Lighter color for reversed button text
+  errorColor: Colors.red[700]!,  // Deeper red for error
+  positiveColor: Colors.green[700]!,  // Deeper green for positive feedback
+  progressBarBackground: Colors.grey[700]!,  // Darker shade for progress bar
+  positiveColorLight: Colors.green[600]!,
+  positiveColorDark: Colors.green[800]!,
+  backdropBackgroundColor: Colors.black,  // Pure black backdrop
+  bestScoreBackground: Colors.purple[800]!,  // Deep shade for the best score background
+  buttonIndicatorColor: Colors.amber[700]!,  // A darker shade of amber for button indicator
+  tableSurroundColor: Colors.grey[850]!, // Very dark grey for table surrounds
 );
+
 
 
 
